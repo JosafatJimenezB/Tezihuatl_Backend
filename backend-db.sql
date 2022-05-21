c0bc8915a5028c5d9b474a8d4a04c496883596d3
JosafatJimenezB <josa221001@gmail.com>
2022-05-20 19:25:15 -0500

    add: añadiendo instrucciones para correrlo en local

---
+++ backend-db.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-05-2022 a las 02:11:06
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `backend-db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
+  `user` varchar(150) NOT NULL,
+  `rol` varchar(50) NOT NULL,
+  `edad` int(3) NOT NULL,
+  `genero` varchar(30) NOT NULL,
+  `estadoCivil` varchar(50) NOT NULL,
+  `domicilio` varchar(400) NOT NULL,
+  `motivo` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `user`, `name`, `pass`) VALUES
+(1, 'admin', 'Administrador', '$2a$08$R4MpQLbDUb3NpvVZsjJMF.xm7vhW90JxfJcncQd6erQop3shnQrSO');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- readme.md
+++ readme.md
@@ -6,36 +6,69 @@
 
 1.- Primero debes clonar este repositorio en tu entorno local pero antes recuerda tener instalado node para poder ejecutarlo y en caso contrario lo puedes descargar del siguiente [enlace](https://nodejs.org/es/).
 
-```
+```bash
     git clone https://github.com/JosafatJimenezB/Tezihuatl_Backend.git
 ```
 
-2.- Instalar las dependencias para ejecutar, este comando ejecutalo dentro de la carpeta del proyecto.
+despues de clonar el repositorio debes dirigirte a la carpeta donde esta el proyecto y ejecutar el siguiente comando:
 
+```bash
+    cd Tezihuatl_Backend
 ```
+
+2.- Teniendo el paso anterior hay que instalar las dependencias de node para ejecutar el proyecto de forma local (verifica estar en la carpeta del proyecto):
+
+```bash
     npm install
 ```
 
 3.- Importa la base de datos a phpMyAdmin
 
-- dirigete al http://localhost/phpmyadmin/index.php
-- Dirigete a la opcion de importar y sube el archivo de la base de datos nombrado **login_node_jwt.sql**.
+Para ello lo primero es importar la base de datos en tu servidor sql o phpmyadmin.
 
-con esto ya tendremos la base de datos operativa
+con esto ya tendremos la base de datos operativa con las siguientes tablas:
+
+**Clients**
+En esta tabla es donde se guardaran los datos de los clientes que se registran en el sistema.
+
+```sql
+    CREATE TABLE `clients` (
+      `id` int(11) NOT NULL,
+      `user` varchar(150) NOT NULL,
+      `rol` varchar(50) NOT NULL,
+      `edad` int(3) NOT NULL,
+      `genero` varchar(30) NOT NULL,
+      `estadoCivil` varchar(50) NOT NULL,
+      `domicilio` varchar(400) NOT NULL,
+      `motivo` varchar(1000) NOT NULL
+    )
+```
+
+**users**
+En esta tabla se van guardando los usuarios que acceden al sistema.
+
+```sql
+    CREATE TABLE `users` (
+      `id` int(11) NOT NULL,
+      `user` varchar(50) NOT NULL,
+      `name` varchar(100) NOT NULL,
+      `pass` varchar(255) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+```
 
 4.- Ejecuta el siguiente comando en la terminal para ejecutar el servidor
 
-```
+```bash
     npm run app.js
 ```
 
 o con el siguiente comando de nodemon para autorefrescar el servidor al hacer cambios
 
-```
+```bash
     nodemon start
 ```
 
-## ##Vistas del proyecto
+## Vistas del proyecto
 
 ---
 
